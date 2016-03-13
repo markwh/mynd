e71f1e21fce4b501cebfe6d75281e495b73fdf76
markwh <mark.hagemann@gmail.com>
2016-03-13 18:55:07 -0400

    purged old and useless files

--- logtasks.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-# Check for differences between files in $TASKDIR directory with $TASKFIL in
-# name and log in $TASKLOG
-
-# For each of the new files, strip away anything without icon BUILTIN="checked"
